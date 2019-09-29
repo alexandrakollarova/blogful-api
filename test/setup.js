2f9dffa0ffa9c2bc2191673d931863771fe09e63
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-09-29 14:25:23 -0400

    initial commit

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,9 @@
-const { expect } = require('chai')
-const supertest = require('supertest')
-
+const setTZ = require('set-tz')
+setTZ('UTC')
+process.env.TZ = 'UTC'
+require('dotenv').config()
+const { expect } = require('chai') // assertion library
+const supertest = require('supertest') // testing HTTP calls
+ 
 global.expect = expect
 global.supertest = supertest
