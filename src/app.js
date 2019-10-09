961fba685fa2ded41d144882712f1010dc2fa2a6
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-10-09 06:20:51 -0400

    last commits

--- src/app.js
+++ src/app.js
@@ -4,40 +4,21 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const ArticlesService = require('./articles-service')
+const usersRouter = require('./users/users-router')
+const commentsRouter = require('./comments/comments-router')
+const articlesRouter = require('./articles/articles-router')
 
 const app = express()
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
 app.use(helmet())
 
-app.get('/articles', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getAllArticles(knexInstance)
-    .then(articles => {
-      res.json(articles)
-    })
-    .catch(next)
-})
-
-app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-      if (!article) {
-        return res.status(404).json({
-          error: { message: `Article doesn't exist` }
-        })
-      }
-      res.json(article)
-    })
-    .catch(next)
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.use('/api/articles', articlesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/comments', commentsRouter)
 
 app.use(function errorHandler(error, req, res, next) {
   let response
